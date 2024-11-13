881e8e8017ab6f9ad687a360770f5f7e95bd9a40
ChangIN76 <vel5535@naver.com>
2024-11-13 21:46:29 +0900

    Add files via upload

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,20 +1,126 @@
-﻿// Program.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+template <typename T>
+class SingleLinkedList
+{
+private:
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    int size;
+    Node* head;
+
+public:
+    SingleLinkedList();
+    {
+        size = 0;
+        head = nullptr;
+    };
+
+    void PushFront(T data)
+    {
+       if (head == nullptr)
+       {
+           head = new Node;
+           
+           head->data = data;
+           head->next = nullptr;
+       }
+       else
+       {
+           Node* newNode = new Node;
+
+           newNode->data = data;
+           newNode->next = head;
+
+           head = newNode;
+       }
+
+       size++;
+    }
+
+    void PopFront()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+
+            head = deleteNode->Next;
+
+            delete
+        }
+
+    }
+
+    void PushBack(T data)
+    {
+        if (head == nullptr)
+        {
+            head = new Node;
+
+            head->data = data;
+            head->next = nullptr;
+        }
+        else
+        {
+            Node* currentNode = head;
+
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            Node* newNode = new Node;
+
+            currentNode->next = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+
+        size++;
+    }
+
+    void Show()
+    {
+        Node* currentNode = head;
+
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " ";
+            currentNode = currentNode->next;
+        }
+    }
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SingleLinkedList<int> singleLinkedList;
+
+    singleLinkedList.PushFront(10);
+    singleLinkedList.PushFront(20);
+
+    // [20] - [10]
+
+    singleLinkedList.PushBack(5);
+    singleLinkedList.PushBack(0);
+
+    // [20] - [10] - [5] - [0]
+
+    singleLinkedList.Show();
+
+    singleLinkedList.PopFront();
+    singleLinkedList.PopFront();
+
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
